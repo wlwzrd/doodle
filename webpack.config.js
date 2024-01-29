908c112c6e92c9f47ebc47c9989f8df41e4a41ca
Mario Fernando <mfdogutierrez@gmail.com>
2024-01-29 09:57:43 -0500

    Update node version and sass

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
+                use: ['style-loader', 'css-loader', 'sass-loader','sass'],
             },
         ],
     },
